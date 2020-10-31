156a9ab87c6287e98ece3981d4a6b7c099f2d943
deeper-x <albertodeprezzo@gmail.com>
2020-10-31 22:39:10 +0100

    added test

--- main_test.go
+++ main_test.go
@@ -7,10 +7,18 @@ import (
 	gomock "github.com/golang/mock/gomock"
 )
 
-func TestOpen(t *testing.T) {
+func TestWriteLine(t *testing.T) {
 	ctrl := gomock.NewController(t)
 
 	rh := NewMockFileManager(ctrl)
 
 	rh.EXPECT().WriteLine("100").Return(errors.New("demo"))
+}
+
+func TestDeleteLine(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	rh := NewMockFileManager(ctrl)
+
+	rh.EXPECT().DeleteLine("100").Return(errors.New("demo"))
 }
